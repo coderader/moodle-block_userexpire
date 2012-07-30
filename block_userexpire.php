124a372c5b93d68768f0e501cd6352ac4f698c5c
Jeff Rader <jrader@sibi.cc>
2012-07-30 14:21:02 -0500

    Initial Git version

---
+++ block_userexpire.php
@@ -0,0 +1,67 @@
+<?php //$Id: block_userexpire.php,v 0.3 2012-07-30 22:00:00 jrader Exp $
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package    moodlecore
+ * @subpackage block
+ * @copyright  2012 Jeff Rader - Sunset Online
+ * @author     2012 Jeff Rader <jrader@sibi.cc>
+ * @version    0.3
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_userexpire extends block_base {
+
+    function init() {
+        $this->title = get_string('pluginname','block_userexpire');
+    }
+
+    function get_content() {
+        global $CFG, $OUTPUT, $USER, $course, $DB;
+        
+        require_once($CFG->dirroot.'/message/lib.php');
+        
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass;
+        $this->content->text = '';
+
+        if (isloggedin() && is_object($course)) {
+			if ($course->id != SITEID) {
+				$sql = 'SELECT ue.id, ue.timestart, ue.timeend
+					FROM mdl_user_enrolments ue
+					JOIN mdl_enrol e on ue.enrolid = e.id
+					WHERE ue.userid = ? AND e.courseid = ?';
+			   
+				$records = $DB->get_records_sql($sql, array($USER->id, $course->id));
+				$student = reset($records);
+				if (isset($student->timeend) && $student->timeend>0) {
+					$this->title = "Your Enrollment";
+					$this->content->text .= get_string('expirelabel', 'block_userexpire').": ".date(get_string('strftimedate', 'block_userexpire'),$student->timeend);
+				}
+			}
+       }
+
+        $this->content->footer = '';
+        return $this->content;
+    }
+    
+}
+?>
---
+++ lang/en/block_userexpire.php
@@ -0,0 +1,8 @@
+<?php
+    
+    $string['pluginname'] = 'User Expire block'; 
+    $string['userinfo'] = 'User Expire Information';
+    $string['strftimedate'] = 'F j, Y';//'l jS \of F j, Y h:i:s A';
+	$string['expirelabel'] = 'Enrollment Expires';
+    
+?>
