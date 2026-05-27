98f416bafeea819072dbca2ecf68b19641b1a128
Jeff Rader <jrader@sibi.cc>
2026-05-27 15:12:11 -0500

    Update block_userexpire.php
    Staring to fix problems

--- block_userexpire.php
+++ block_userexpire.php
@@ -1,6 +1,4 @@
-<?php //$Id: block_userexpire.php,v 1.0 2016-02-22 22:00:00 jrader Exp $
-
-// This file is part of Moodle - http://moodle.org/
+<?php// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,42 +15,50 @@
 
 /**
  *
- * @package    moodlecore
+ * @package    user_expire
  * @subpackage block
- * @copyright  2023 Jeff Rader - Sunset Online
+ * @copyright  2026 Jeff Rader - Sunset Online
  * @author     Jeff Rader <jrader@sibi.cc>
- * @version    1.2
+ * @version    1.3
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
-
-
+/**
+ * The only class for the plugin.
+ *
+ * @package    mod_userexpire
+ * @copyright  2026 Jeff Rader
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class block_userexpire extends block_base {
-
-    function init() {
-        $this->title = get_string('pluginname','block_userexpire');
+/**
+ * @inherited docs.
+ *
+ * @return string of the plugin name
+ */
+	function init() {
+        $this->title = get_string('pluginname', 'block_userexpire');
     }
-
+/**
+ * @inherited docs.
+ *
+ * @return string of the output for the block
+ */
     function get_content() {
-        global $CFG, $OUTPUT, $USER, $course, $DB;
-        
+	    global $CFG, $OUTPUT, $USER, $course, $DB;
         require_once($CFG->dirroot.'/message/lib.php');
-        
-        if ($this->content !== NULL) {
+        if ($this->content !== null) {
             return $this->content;
         }
-
         $this->content = new stdClass;
         $this->content->text = '';
-
         if (isloggedin() && is_object($course)) {
 			if ($course->id != SITEID) {
 				$sql = "SELECT ue.id, ue.timestart, ue.timeend
 					FROM {user_enrolments} ue
 					JOIN {enrol} e on ue.enrolid = e.id
 					WHERE ue.userid = ? AND e.courseid = ?";
-			   
 				$records = $DB->get_records_sql($sql, array($USER->id, $course->id));
 				$student = reset($records);
 				if (isset($student->timeend) && $student->timeend>0) {
@@ -75,6 +81,11 @@ class block_userexpire extends block_base {
         $this->content->footer = '';
         return $this->content;
     }
+/**
+ * @inherited docs.
+ *
+ * @return array to designate where the block anca be used.
+ */
     function applicable_formats() {
 	// Default case: the block can be used in courses and site index, but not in activities
 		return array(
