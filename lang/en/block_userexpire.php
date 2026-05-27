ea083409a260e4ce6548b5ba519c77a0c4c43a11
Jeff Rader <jrader@sibi.cc>
2026-05-27 18:38:50 -0500

    Update block_userexpire.php

--- lang/en/block_userexpire.php
+++ lang/en/block_userexpire.php
@@ -1,12 +1,33 @@
 <?php
-    
-    $string['pluginname'] = 'User Expire block'; 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @file
+ * @package    block_userexpire
+ * @copyright  2026 Jeff Rader - Sunset Online
+ * @author     Jeff Rader <jrader@sibi.cc>
+ * @version    1.3
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+    $string['pluginname'] = 'User Expire block';
+    $string['expirelabel'] = 'Enrollment Expires';
+    $string['strftimedate'] = 'F j, Y';
     $string['userinfo'] = 'User Expire Information';
-    $string['strftimedate'] = 'F j, Y';//'l jS \of F j, Y h:i:s A';
-	$string['expirelabel'] = 'Enrollment Expires';
-	$string['expiretitle'] = 'Your Enrollment';
-	$string['expireday'] = 'days';
-	$string['expirehours'] = 'hours';
-	$string['enrolltitle'] = "Enroll in this Course";
-	$string['enrolltext'] = "Enroll me in this course";
-?>
+    $string['expireday'] = 'days';
+    $string['expiretitle'] = 'Your Enrollment';
+    $string['enrolltext'] = "Enroll me in this course";
+    $string['enrolltitle'] = "Enroll in this Course";
+    $string['expirehours'] = 'hours';
